86c5580d226bcc01abf175a00430bb1dc09622f3
jcanett1 <jcanett@pxg.com>
2026-01-30 13:35:12 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react' // o el plugin que uses
 
 export default defineConfig({
-  base: '/Retrabajos-Kitteo/', // <- importante para GitHub Pages (project site)
+  base: '/Retrabajos-Kitteo/',
   plugins: [react()],
 })
